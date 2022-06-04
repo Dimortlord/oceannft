72a51fcc3b48050923b44ec2fdc3aa80fcc3d12b
Dmitry Zinchenko <102792310+Dimortlord@users.noreply.github.com>
2022-06-04 10:31:16 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  spaceBetween: 10,
+
+  breakpoints: {
+    320: {
+      centeredSlides: true,
+    },
+    768: {
+      slidesPerView: false,
+    },
+    1200: {
+      slidesPerView: true,
+    },
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
