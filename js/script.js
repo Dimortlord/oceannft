6146fda13d7d452ace139ca3e4432adae93f12d3
Dmitry Zinchenko <dimortlord@gmail.com>
2022-06-04 14:18:58 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,32 +1,6 @@
-new Swiper(".swiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  spaceBetween: 10,
-
-  breakpoints: {
-    320: {
-      centeredSlides: true,
-    },
-    768: {
-      slidesPerView: false,
-    },
-    1200: {
-      slidesPerView: true,
-    },
-  },
-
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 1,
-    },
-    1200: {
-      slidesPerView: 3,
-    },
-  },
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__menu").toggleClass("active");
+    $(".body").toogleClass("lock");
+  });
 });
